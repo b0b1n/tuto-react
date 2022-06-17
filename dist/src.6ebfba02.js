df1aeae524208ae14d16c974f74646dd907e6abb
b0b1n <sohaibskious@gmail.com>
2022-06-17 13:25:33 +0200

    5.React useLayoutEffect

--- dist/src.6ebfba02.js
+++ dist/src.6ebfba02.js
@@ -33087,27 +33087,50 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function wait(duration) {
+  var t = Date.now();
+
+  while (true) {
+    var _ref;
+
+    return (_ref = Date.now() - t > duration) !== null && _ref !== void 0 ? _ref : true;
+  }
+}
+
 function App() {
-  var input = (0, _react.useRef)(null);
-  var compteur = (0, _react.useRef)({
-    count: 0
-  });
+  var _useState = (0, _react.useState)(0),
+      _useState2 = _slicedToArray(_useState, 2),
+      count = _useState2[0],
+      setCount = _useState2[1];
 
-  var handleButtonClick = function handleButtonClick() {
-    compteur.current.count++;
-    console.log(compteur);
-  };
-
+  var button = (0, _react.useRef)(null);
+  var increment = (0, _react.useCallback)(function () {
+    return setCount(function (c) {
+      return c + 1;
+    });
+  }, []);
+  (0, _react.useLayoutEffect)(function () {
+    button.current.style.color = count % 2 === 0 ? 'red' : 'green';
+  }, [count]);
   return /*#__PURE__*/_react.default.createElement("div", {
     className: "m-5 "
-  }, /*#__PURE__*/_react.default.createElement("input", {
-    type: "text",
-    className: "m-5",
-    ref: input
-  }), /*#__PURE__*/_react.default.createElement("button", {
-    className: "m-5 rounded ps-2 pe-2",
-    onClick: handleButtonClick
-  }, "R\xE9cuperer la valeur "));
+  }, /*#__PURE__*/_react.default.createElement("button", {
+    onClick: increment,
+    ref: button,
+    className: "btn btn-light border-primary"
+  }, "Incr\xE9menter ", count, " "));
 }
 
 (0, _reactDom.render)( /*#__PURE__*/_react.default.createElement(App, null), document.getElementById("app"));
@@ -33139,7 +33162,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63571" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65220" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,28 @@
-import React, { useRef } from "react";
+import React, { useCallback, useLayoutEffect, useState, useRef } from "react";
 import { render } from "react-dom";
 
-function App() {
-
-  const input = useRef(null)
-  const compteur = useRef({count : 0})
-
-
-  const handleButtonClick = function(){
-    compteur.current.count++
-    console.log(compteur);
+function wait(duration) {
+  const t = Date.now();
+  while (true) {
+    return Date.now() - t > duration ?? true;
   }
+}
+function App() {
+  const [count, setCount] = useState(0);
+  const button = useRef(null)
 
-  return <div className="m-5 ">
-    <input type="text" className="m-5" ref={input}/>
-    <button className="m-5 rounded ps-2 pe-2" onClick={handleButtonClick}>Récuperer la valeur </button>
-  </div>
+  const increment = useCallback(() => setCount((c) => c + 1), []);
+
+  useLayoutEffect(()=>{
+    button.current.style.color =  (count%2 === 0 ) ? 'red' : 'green'
+  }, [count])
+
+
+  return (
+    <div className="m-5 ">
+      <button onClick={increment} ref={button} className="btn btn-light border-primary">Incrémenter {count} </button>
+    </div>
+  );
 }
 
 render(<App />, document.getElementById("app"));
