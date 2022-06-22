86dcd2cc45e22d52d2a5d0fac012487a771c5024
b0b1n <sohaibskious@gmail.com>
2022-06-22 05:36:29 +0200

    9.React manipulating child components, use of .toArray & .cloneElement

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Tab, Tabs } from "./Tabs";
+
+function App() {
+  return (
+    <Tabs >
+      <Tab title="First Tab">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui hic aliquid
+        ducimus, quia voluptas incidunt esse, velit quos molestiae beatae, a
+        ipsam? Ipsa facere tempora exercitationem amet, inventore modi dolorum?
+      </Tab>
+      <Tab title="Second Tab">
+        Consectetur adipisicing elit. Qui hic aliquid ducimus, quia voluptas
+        incidunt esse, velit quos molestiae beatae, a ipsam? Ipsa facere tempora
+        exercitationem amet, inventore modi dolorum?
+      </Tab>
+    </Tabs>
+  );
+}
+
+export default App;
